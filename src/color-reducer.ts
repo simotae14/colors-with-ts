7bb8a7f3028556593c5a05f88e0ad21d87689dde
Simone Taeggi <startae14@gmail.com>
2023-05-09 21:51:58 +0200

    define a file with the colorReducer

---
+++ src/color-reducer.ts
@@ -0,0 +1,48 @@
+import { rgb } from 'color-convert';
+
+export type UpdateHexColorAction = {
+  type: 'update-hex-color';
+  payload: {
+    hexColor: string;
+  };
+};
+
+type UpdateRGBColorAction = {
+  type: 'update-rgb-color';
+  payload: {
+    rgb: [number, number, number];
+  };
+};
+
+type ColorState = {
+  hexColor: string;
+};
+
+export const initialState: ColorState = {
+  hexColor: '#BADA55',
+};
+
+const colorReducer = (
+  state: ColorState = initialState,
+  action: UpdateHexColorAction | UpdateRGBColorAction,
+) => {
+  if (action.type === 'update-hex-color') {
+    const { hexColor } = action.payload;
+    return {
+      ...state,
+      hexColor,
+    };
+  }
+
+  if (action.type === 'update-rgb-color') {
+    const hexColor = rgb.hex(action.payload.rgb);
+    return {
+      ...state,
+      hexColor,
+    };
+  }
+
+  return state;
+};
+
+export default colorReducer;
